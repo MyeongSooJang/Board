0d6d219825e3944a7eaa2e0a57598c752af029b1
MyeongSooJang <mmsgod0427@naver.com>
2025-12-30 16:38:47 +0900

    feat: MySQL DB 연결 && JPA 설정
    - 환경변수에 USERNAME, PASSWORD 등록
    - JPA SQL문이 출력 되도록 설정

--- src/test/java/ms/board/BoardApplicationTests.java
+++ src/test/java/ms/board/BoardApplicationTests.java
@@ -1,13 +1,8 @@
 package ms.board;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BoardApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
